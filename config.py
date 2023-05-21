4fec8867ba07d7f3e71c6f1937e67a6ac18dec07
jagernau <stepanenkov25@gmail.com>
2023-05-21 21:59:16 +0300

    написал рассыльщик писем

--- config.py
+++ config.py
@@ -11,3 +11,7 @@ DB_USER: str = str(env_dict["DB_USER"])
 DB_PASSWORD: str = str(env_dict["DB_PASSWORD"])
 DB_HOST: str = str(env_dict["DB_HOST"])
 DB_NAME: str = str(env_dict["DB_NAME"])
+
+EMAIL_SENDER: str = str(env_dict["EMAIL_SENDER"])
+EMAIL_PASSWORD: str = str(env_dict["EMAIL_PASSWORD"])
+SMTP_SERVER: str = str(env_dict["SMTP_SERVER"])
